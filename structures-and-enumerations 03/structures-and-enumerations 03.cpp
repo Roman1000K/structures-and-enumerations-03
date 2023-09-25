129910be5d0e02c4cd85089f05af950c94742032
Rom <kornev.roma1881@gmail.com>
2023-09-25 11:03:23 +0300

    Добавьте файлы проекта.

---
+++ structures-and-enumerations 03/structures-and-enumerations 03.cpp
@@ -0,0 +1,42 @@
+﻿#include <Windows.h>
+#include <iostream>
+ 
+struct address {
+    std::string City;
+    std::string Street;
+    int HouseNumber;
+    int ApartmentNumber;
+    int Index;
+};
+void print(std::string City, std::string Street, int HouseNumber, int ApartmentNumber, int Index) 
+{
+    std::cout << "Ваш адрес: Город " << City << ", " << "улица " << Street << ", " << "номер дома " << HouseNumber << ", "
+        << "номер квартиры " << ApartmentNumber << ", " << "почтовый индекс " << Index;
+    }
+
+int main()  
+{    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);    
+     
+    std::cout << "Введите ваш адрес\n";
+    std::cout << "Город: ";
+    std::string City;
+    std::cin >> City;
+    std::cout << "Улица: ";
+    std::string Street;
+    std::cin >> Street;
+    std::cout << "Номер дома: ";
+    int HouseNumber;
+    std::cin >> HouseNumber;
+    std::cout << "Номер квартиры: ";
+    int ApartmentNumber;
+    std::cin >> ApartmentNumber;
+    std::cout << "Почтовый индекс: ";
+    int Index;
+    std::cin >> Index;
+
+    address address1 = { City, Street, HouseNumber, ApartmentNumber, Index };
+
+    print(City, Street, HouseNumber, ApartmentNumber, Index);
+}
